abb3270a69e6978f6d7eed29e65cf958b851f88b
vxvinod <vinodkmrmurugan@gmail.com>
2014-07-08 08:52:00 +0530

    simple cms crud operation done

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,54 @@
+class SubjectsController < ApplicationController
+
+  layout false
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new(:name => "Default")
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    if(@subject.save)
+      redirect_to(:action => "index")
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+      redirect_to(:action => "show", :id => @subject.id)
+    else
+      render "update"
+    end
+  end
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id])
+    subject.destroy
+    redirect_to(:action => 'index')
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name,:position,:visible)
+  end
+
+end
